da687d2216553c0f9e6325d563a88a8ddb8cbe0c
unknown <emile.bernard.2@ulaval.ca>
2019-02-26 21:12:20 -0500

    Added documentation images and cleaned up code

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # OpenCV-Webcam-Stream
 This project contains a basic python script to run opencv-python with a webcam stream.
 
+## How it work's
+
+Haar-Features are good at detecting edges and lines, this makes it effective in face detection techniques. Also, Haar-based classifiers typically involve less computations so it has a higher execution speed.
+
+Here are some Haar-Features types:
+
+![haar_features_types](./documentation/haar_features_types.jpg?raw=true"haar_features_types")
+
+Basic line features looks like this:
+
+![haar_features](./documentation/haar_features.png?raw=true"haar_features")
+
+Numericaly, it looks like this:
+
+![numerical_haar_features](./documentation/numerical_haar_features.png?raw=true"numerical_haar_features")
+
+Haar-feature would be able to detect eyes on a face because the eyes are an area that is dark on top and brighter underneath):
+
+![haar](./documentation/haar.png?raw=true"haar")
+
 ## How to setup
 - Install opencv-python
 ```
--- opencv-webcam.py
+++ opencv-webcam.py
@@ -4,9 +4,17 @@ import sys
 videoCapture = cv2.VideoCapture(0)
 pretrainedFaceClassifier = cv2.CascadeClassifier("./assets/model/haarcascade_frontalface_alt.xml")
 
-def drawRectagle(frame, objects):
+def drawText(frame, text, position):
+    font = cv2.FONT_HERSHEY_DUPLEX
+    scale = 1
+    color = (255, 255, 255)
+    thickness = 1
+    cv2.putText(frame, text, position, font, scale, color, thickness)
+
+def drawDetectedObjects(frame, objects):
     for (x, y, w, h) in objects:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+        drawText(videoFrame, "Face", (x, y-10))
 
 def releaseCapture():
     videoCapture.release()
@@ -22,7 +30,9 @@ while True:
         minSize=(20, 20),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-    drawRectagle(videoFrame, detectedObjects)
+    drawDetectedObjects(videoFrame, detectedObjects)
+
+
     cv2.imshow('OpenCV face recogniton applied to a webcam video stream', videoFrame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
